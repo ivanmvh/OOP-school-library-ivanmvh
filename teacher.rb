700428128386916948fcaa6bcaa04060160f1ccf
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-03-15 03:44:30 -0500

    Create class Teacher - teacher.rb

---
+++ teacher.rb
@@ -0,0 +1,18 @@
+require './person'
+
+# Class - Teacher
+class Teacher < Person
+
+  def initialize(*person, specialization)
+    super(*person)
+    @specialization = specialization
+  end
+
+  # Can_use_services always return true for teachers
+  def can_use_services?
+    true
+  end
+
+  # getter
+  attr_accessor :specialization
+end
