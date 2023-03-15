c7292572355dbdd97997589f1750db7938cfc6b1
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-03-15 03:06:07 -0500

    Create class Person - person.rb
    - Instance vars: @id, @name, and @age.
    - Constructor with name, age, and parent_permission as parameter. name and parent_permission are optional and have default values of "Unknown" and true.
    - Getters for @id, @name, and @age.
    - Setters for @name and @age.
    - Private method is_of_age? that returns true if @age is greater or equal to 18 and false otherwise.
    - Public method can_use_services? that returns true if person is of age or if they have permission from parents.

---
+++ person.rb
@@ -0,0 +1,33 @@
+# Class Person
+class Person
+  
+  # getters 
+  attr_reader :id, :age, :name, :parent_permission
+  attr_accessor :name, :age
+
+  def intialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+  
+  # Methods
+  # Public
+  def can_use_services?
+    is_of_age? || @parent_permission
+  end
+  
+  private
+  def of_age?
+    @age >= 18
+  end
+end
+
+=begin
+# test
+ivan = Person.new(73, "Ivan Martinez",true)
+puts ivan.age
+puts ivan.name
+ivan.parent_permission
+=end
