014cda08f140a8b8303b62dfbef75480a3e07c7a
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-03-16 08:46:54 -0500

    Add line

--- rental.rb
+++ rental.rb
@@ -3,6 +3,7 @@ require './book'
 # intermediate class `Rental - relationship between `Person` and `Book``
 class Rental
   attr_accessor :date, :book, :person
+  
   def initialize(date, book, person)
     @date = date
     @person = person
