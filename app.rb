6860e3009bd90a1b5a7e4642a62d3eb1fd548de0
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-03-16 23:42:17 -0500

    Add create_rental and list_all_rentals_for_person

--- app.rb
+++ app.rb
@@ -114,4 +114,39 @@ class App
     puts 'Person created successfully'
     @people.push(teacher)
   end
+
+  def create_book
+    print 'Title: '
+    title = gets.chomp
+    print 'Author: '
+    author = gets.chomp
+    book = Book.new(title, author)
+    puts 'Book created successfully'
+    @books.push(book)
+  end
+
+  def create_rental
+    puts 'Select a book from the following list by number'
+    @books.each_with_index { |book, index| puts "#{index}) Title: #{book.title}, Author: #{book.author}" }
+    book_index = gets.chomp.to_i
+    puts 'Select a person from the following list by number (not id)'
+    @people.each_with_index do |person, index|
+      puts "#{index}) [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+    end
+    person_index = gets.chomp.to_i
+    print 'Date: '
+    date = gets.chomp
+    rental = Rental.new(date, @books[book_index], @people[person_index])
+    @rentals << rental unless @rentals.include?(rental)
+    puts 'Rental created successfully'
+  end
+
+  def list_all_rentals_for_person_id
+    print 'ID of person: '
+    id = gets.chomp.to_i
+    puts 'Rentals:'
+    @rentals.select do |rental|
+      puts "Date: #{rental.date}, Book '#{rental.book.title}' by #{rental.book.author}" if rental.person.id == id
+    end
+  end
 end
