1afda751afea071634361258ba7cf9320b7eae76
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-03-16 20:36:25 -0500

    Create create_book and list_all_books methods

--- app.rb
+++ app.rb
@@ -43,9 +43,29 @@ class App
         break
       elsif option
         send(option)
+        p "Pulse any key to continue..."
+        option = gets.chomp.to_i
       else
         puts 'That is not a valid input'
       end
     end
   end
+end
+
+def list_all_books
+  if @books.empty?
+    puts 'No books found'
+  else
+    @books.each { |book| puts "Title: #{book.title}, Author: #{book.author}" }
+  end
+end
+
+def create_book
+  print 'Title: '
+  title = gets.chomp
+  print 'Author: '
+  author = gets.chomp
+  book = Book.new(title, author)
+  puts 'Book created successfully'
+  @books.push(book)
 end
